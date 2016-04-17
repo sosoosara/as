01fd5f747bb58a89889894c3c422a31c54471a08
sara <sf088995@gmail.com>
2016-04-17 12:55:24 +0200

    virsion1

---
+++ index.blade.php
@@ -0,0 +1,19 @@
+@extends('master')
+@section('content')
+<form>
+<table class="table">
+<tr>
+<th>id</th>
+<th>first name</th>
+<th>last name</th>
+</tr>
+<tr>
+@foreach($team as $member)
+<td>{{$member->id}}</td>
+<td>{{$member->firstname}}</td>
+<td>{{$member->lastname}}</td>
+</tr>
+@endforeach
+</table>
+<form>
+@stop
