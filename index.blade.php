e48051e674efee98ebc65cab9a7ee9e91989f727
sara <sf088995@gmail.com>
2016-04-17 13:21:22 +0200

    virsion1

--- index.blade.php
+++ index.blade.php
@@ -6,12 +6,14 @@
 <th>id</th>
 <th>first name</th>
 <th>last name</th>
+<th>password</th>
 </tr>
 <tr>
 @foreach($team as $member)
 <td>{{$member->id}}</td>
 <td>{{$member->firstname}}</td>
 <td>{{$member->lastname}}</td>
+<td>{{$member->password}}</td>
 </tr>
 @endforeach
 </table>
